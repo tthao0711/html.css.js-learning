054a5a5134e64d797e89464a2193252699590d14
Thao Luu <thaoluu0969@gmail.com>
2025-09-03 19:17:14 -0500

    Add random password generator JS exercise

--- html-css-js-learning/dice-roller-js-learning/index.js
+++ html-css-js-learning/dice-roller-js-learning/index.js
@@ -11,7 +11,7 @@ function rollDice(){
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
-        images.push(`<img src="dice-images/dice-${value}.png">`);
+        images.push(`<img src="dice-images/dice-${value}.png" alt="Dice ${value}">`);
     }
 
     diceResult.textContent = `dice: ${values.join(', ')}`;
---
+++ html-css-js-learning/random-password-generator-js-learning/index.js
@@ -0,0 +1,43 @@
+// RANDOM PASSWORD GENERATOR
+
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwyz"
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+    const numberChars = "0123456789"
+    const symbolChars = "!@#$%^&*()~+-="
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <= 0){
+        return `(password length must be at least 1)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At least 1 set of character needs to be selected)`
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+
+    return '';
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
