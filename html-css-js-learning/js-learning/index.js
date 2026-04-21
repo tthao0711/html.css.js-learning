3b14bcfb0af468848b4d250537367d0997c88466
Thao Luu <thaoluu0969@gmail.com>
2026-04-21 14:32:09 -0500

    Add rock-paper-scissors program

--- html-css-js-learning/js-learning/index.js
+++ html-css-js-learning/js-learning/index.js
@@ -3,14 +3,3 @@
 
 
 
-try{
-    const dividend = window.prompt("Enter a dividend:");
-    const divisor = window.prompt("Enter a divisor:");
-
-    const result = dividend / divisor;
-    console.log(result);
-}
-
-catch(error){
-    console.log(error);
-}
