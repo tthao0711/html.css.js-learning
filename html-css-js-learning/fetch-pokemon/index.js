0f02eb9891a2461f99475529b6d023325379cead
Thao Luu <thaoluu0969@gmail.com>
2026-04-21 17:56:04 -0500

    Add first fetch data program

---
+++ html-css-js-learning/fetch-pokemon/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+window.fetchData = async function() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
